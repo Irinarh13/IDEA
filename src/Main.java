7709ab17b579c8dfe8cead41f9267bebe6ba6847
Irinarh <jagdtiger_86@mail.ru>
2024-02-24 19:58:00 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 10000; // билет
+        int mile = 20; // миля
+
+        System.out.println("Количество начисленных миль за купленный билет.");
+        System.out.println((ticket / mile) + " миля(ли/ль)");
+
+    }
+}
